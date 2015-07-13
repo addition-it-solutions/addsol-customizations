fa5f475c5b23ae4bd31dd006e27da24fb1a47e04
Ujjvala-Addsol <ujjvalag@aitspl.com>
2015-07-13 12:31:45 +0530

    Search & get resources based on request criteria.

--- addsol_capgemini_resource/wizard/wizard_assign_resources.py
+++ addsol_capgemini_resource/wizard/wizard_assign_resources.py
@@ -33,12 +33,40 @@ class assign_resources(osv.osv_memory):
         'resource_ids' : fields.many2many('hr.employee','res_request_assign_rel','employee_id','assign_id', 'Assigned Resources'),
     }
     
+    def default_get(self, cr, uid, fields, context=None):
+        if context is None:
+            context = {}
+        result = super(assign_resources, self).default_get(cr, uid, fields, context=context)
+        if not result.get('resource_ids'):
+            result['resource_ids'] = []
+        request_obj = self.pool.get('resource.request')
+        resource_skills_obj = self.pool.get('resource.skill.set')
+        resource_obj = self.pool.get('hr.employee')
+        record_id = context.get('active_id',False)
+        if record_id:
+            requests = request_obj.browse(cr, uid, record_id, context)
+            resource_ids = []
+            for line in requests.request_line_ids:
+                if line.skill_id and line.level_id:
+                    resource_skill_ids = resource_skills_obj.search(cr, uid, [('skill','=',line.skill_id.id),('level','=',line.level_id.id)])
+                    resource_ids.extend([res.resource.id for res in resource_skills_obj.browse(cr, uid, resource_skill_ids, context) if res.resource.id not in resource_ids])
+                if resource_ids:
+                    for res in resource_obj.browse(cr, uid, resource_ids, context):
+                        if res.billable_end_date >= line.billability_start_date and not res.on_bench:
+                            index = resource_ids.index(res.id)
+                            resource_ids.pop(index)
+                    if 'resource_ids' in fields and resource_ids:
+                        result.update({'resource_ids': resource_ids})
+        return result
+    
     def assign(self, cr, uid, ids, context=None):
-        resource_obj = self.pool.get('resource.request')
-        for assign_id in self.browse(cr, uid, ids):
-            if assign_id:
-                res_ids = [res.id for res in assign_id.resource_ids]
-                resource_obj.write(cr, uid, context['active_id'],{'resource_ids': [(4, res_id) for res_id in res_ids],'state':'assign'},context=context)
+        request_obj = self.pool.get('resource.request')
+        record_id = context.get('active_id',False)
+        if record_id:
+            for assign_id in self.browse(cr, uid, ids):
+                if assign_id:
+                    res_ids = [res.id for res in assign_id.resource_ids]
+                    request_obj.write(cr, uid, record_id,{'resource_ids': [(4, res_id) for res_id in res_ids],'state':'assign'},context=context)
         return True
     
     
