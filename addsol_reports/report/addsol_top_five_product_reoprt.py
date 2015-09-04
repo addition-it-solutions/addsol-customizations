138035625c40e513c06988cd8fd1429a4ee64011
Ujjvala-Addsol <rupeshg@aitspl.com>
2015-09-04 15:39:14 +0530

    Create module for reports contain Productwise sale report

---
+++ addsol_reports/report/addsol_top_five_product_reoprt.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015-2016 Addition IT Solutions Pvt. Ltd. (<http://www.aitspl.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+#from openerp.osv import fields, osv
+from openerp import models, fields, api, _
+from openerp import tools
+
+
+class top_five_product_report(models.Model):
+    _name = "top.five.product.report"
+    _description = ""
+    _auto = False
+    
+    name_template = fields.Char("Product Name")
+    amount_total = fields.Float("Invoice Total")
+    product_id = fields.Many2one('product.product', "Product Id")
+
+    def init(self, cr):
+        tools.sql.drop_view_if_exists(cr, 'top_five_product_report')
+        cr.execute("""
+            CREATE view top_five_product_report as
+              SELECT 
+                    MIN(inv.id) as id,
+                    SUM(inv.amount_total) as amount_total, 
+                    invl.product_id as product_id,
+                    product.name_template as name_template
+              FROM 
+                   account_invoice inv
+              JOIN account_invoice_line invl ON invl.invoice_id = inv.id
+              JOIN product_product product ON product.id = invl.product_id
+              GROUP BY 
+                  product.name_template,invl.product_id
+              ORDER BY 
+                  amount_total DESC
+        """)
