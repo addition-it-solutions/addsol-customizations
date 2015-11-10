d5b63c5c1996b1d846df402930d4985c1fc8b497
Ujjvala Gonsalves <ujjvalag@aitspl.com>
2015-11-10 10:38:24 +0530

    [MOVE] Moved modules to deltas-pharma branch.

--- addsol_invoice_tally/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from openerp import models, fields, api
-
-# class addsol_product(models.Model):
-#     _name = 'addsol_product.addsol_product'
-
-#     name = fields.Char()
